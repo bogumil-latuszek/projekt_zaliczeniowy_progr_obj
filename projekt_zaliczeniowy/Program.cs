022faa3cdda7b261bfa29af513cf500f3fa2ed3b
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-06-08 01:54:19 +0200

    utworzenie projektu w vs

---
+++ projekt_zaliczeniowy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projekt_zaliczeniowy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
